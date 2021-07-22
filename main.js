5e4c433dcc61e1ad7519c4d5427436ee148552f9
Yamba <dudgh5002@naver.com>
2021-07-22 17:17:11 +0900

    JS : Navbar menu toggle

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 'use strict'
 
-// make navbar transparent when it is on the top
+// Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () =>{
-    console.log(window.scrollY);
-    console.log(navbarHeight);
+document.addEventListener('scroll', () => {
+    // console.log(window.scrollY);
+    // console.log(navbarHeight);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     }
@@ -14,15 +14,9 @@ document.addEventListener('scroll', () =>{
     }
 });
 
-// handle scrolling when tapping on the navbar menu
-
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: 'smooth' });
-}
-
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event)=>{
+navbarMenu.addEventListener('click', (event) => {
     console.log(event.target.dataset.link);
     const target = event.target;
     const link = target.dataset.link;
@@ -33,11 +27,18 @@ navbarMenu.addEventListener('click', (event)=>{
     // console.log(event.target.dataset.link);
     // const scrollTo = document.querySelector(link);
     // scrollTo.scrollIntoView({behavior: 'smooth', block: 'center'});
-    
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
     // 정리한코드
 
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
 
 // Handle scrolling when tapping on the navbar menu
 
@@ -139,3 +140,7 @@ workBtnContainer.addEventListener('click', (e) => {
 });
 
 
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth' });
+}
